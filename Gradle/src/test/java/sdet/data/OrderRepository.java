67b0065735e04e91f3a5a2ad1dc181f05baf22ec
Kavyashree-g-ust <304556@ust.com>
2026-07-09 11:13:24 +0530

    Initial commit 7

--- Gradle/src/test/java/sdet/data/OrderRepository.java
+++ Gradle/src/test/java/sdet/data/OrderRepository.java
@@ -74,10 +74,14 @@ public class OrderRepository {
         return queryForInt("SELECT COUNT(*) FROM order_statuses");
     }
     public void resetMutableTables() {
-        String sql = "TRUNCATE retail_order_items, retail_orders RESTART IDENTITY CASCADE";
         try (Connection connection = connection();
              Statement statement = connection.createStatement()) {
-            statement.execute(sql);
+
+            statement.execute("SET FOREIGN_KEY_CHECKS = 0");
+            statement.execute("TRUNCATE TABLE retail_order_items");
+            statement.execute("TRUNCATE TABLE retail_orders");
+            statement.execute("SET FOREIGN_KEY_CHECKS = 1");
+
         } catch (SQLException e) {
             throw new IllegalStateException("Could not reset order test data", e);
         }
